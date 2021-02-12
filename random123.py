7e17850d067da6eef80a4c72a847fcede310ace0
Jason Ou <lloivne4119@gmail.com>
2021-02-12 23:56:10 +0800

    edition2

--- random123.py
+++ random123.py
@@ -1,8 +1,9 @@
 import random
 
 ra = random.randint(1, 100)
-
+count = 0
 while True:
+    count += 1 # count = count + 1
     rd = input('猜個1-100 的數字吧: ')
     rd = int(rd)
 
@@ -13,6 +14,8 @@ while True:
             print('比答案小!')
         else:
             print('終於答對了')
+            print('這是你猜的第', count, '次')
             break
+        print('這是你猜的第', count, '次')
     else:
         break
