38924b1476841a4191b254c552ab0b494c04f909
Jason Ou <lloivne4119@gmail.com>
2021-02-13 00:04:13 +0800

    edition2

--- random123.py
+++ random123.py
@@ -1,13 +1,18 @@
 import random
 
-ra = random.randint(1, 100)
+start = input('請決定最小值: ')
+end = input('請決定最大值: ')
+start = int(start)
+end = int(end)
+
+ra = random.randint(start, end)
 count = 0
 while True:
     count += 1 # count = count + 1
-    rd = input('猜個1-100 的數字吧: ')
+    rd = input('猜個你自己決定範圍的數字吧: ')
     rd = int(rd)
 
-    if rd >= 1 and rd <= 100:
+    if rd >= start and rd <= end:
         if rd > ra:
             print('比答案大!')
         elif rd < ra:
@@ -18,4 +23,5 @@ while True:
             break
         print('這是你猜的第', count, '次')
     else:
+        print('輸入在自己設定的範圍內好嗎')
         break
